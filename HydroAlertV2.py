4f834131aed0a3387372e6744f045c5a095240b4
rickkcastro <Heinrickoficial25@gmail.com>
2025-06-05 20:43:34 -0300

    FEAT: Refactor user registration and management functions; add sensor management and alert generation features

--- HydroAlertV2.py
+++ HydroAlertV2.py
@@ -5,7 +5,6 @@ import uuid
 usuarios_cadastrados = []
 sensores_cadastrados = []
 alertas = []
-
 mensagemEmergencia = "Sua região está em alerta de emergência! \nEvacue a área! \nClique no link a seguir para calcular uma rota de fuga. \nhttps://hydroalert.com.br/rotafuga"
 mensagemAtencao = "Sua região está em alerta de atenção! \nProcure abrigo e fique atento às atualizações."
 mensagemObservacao = "Sua região está em alerta de observação! \nFique atento às atualizações."
@@ -16,7 +15,7 @@ def main():
     menu()
 
 def menu():
-    while True:    
+    while True:
         print('\n-----------Menu-----------')
         print('Opção 1: Gestão de usuários')
         print('Opção 2: Gestão de sensores')
@@ -24,20 +23,18 @@ def menu():
         print('Opção 4: Visualizar alertas')
         print('Opção 5: Gerar alerta manual')
         print('Opção 6: Sair do sistema')
-
         opicao = input('\nDigite a sua opção: ')
-
         match opicao:
             case '1':
                 gestaoUsuarios()
             case '2':
                 gestaoDeSensores()
             case '3':
-                ativarSensores()
+                ativarSensoresInput()
             case '4':
-                visualizarAlertas()
+                visualizarAlertasInput()
             case '5':
-                gerarAlertaManual()
+                gerarAlertaManualInput()
             case '6':
                 print("Encerrando o sistema. Até logo!")
                 exit()
@@ -47,25 +44,22 @@ def menu():
 def exibirMensagemInicial():
     print("Bem-vindo ao HydroAlert!")
 
-
-# Usuários
+# Usuários - Frontend
 def gestaoUsuarios():
-    while True:        
+    while True:
         print("\nOpção 1: Cadastrar usuário")
         print("Opção 2: Visualizar usuários")
         print("Opção 3: Remover usuários")
         print("Opção 4: Voltar ao menu principal")
-
         resp = input('\nDigite sua opção: ')
-
         if resp == '1':
             cadastrarUsuarioInput()
             break
         elif resp == '2':
-            visualizarUsuarios()
+            visualizarUsuariosInput()
             break
-        elif resp == '3': 
-            removerUsuario()
+        elif resp == '3':
+            removerUsuarioInput()
             break
         elif resp == '4':
             menu()
@@ -77,11 +71,9 @@ def cadastrarUsuarioInput():
     nome = input('\nDigite seu nome: ')
     endereco = input('Digite seu endereço: ')
     telefone = input('Digite seu telefone: ')
-
     usuario = cadastrarUsuario(nome, endereco, telefone)
     print(f"Usuário {usuario['nome']} cadastrado com sucesso!")
-
-    while True:     
+    while True:
         resp = input('Cadastrar outro usuário (s) ou voltar para o menu (m)?').lower()
         if resp == 's':
             cadastrarUsuarioInput()
@@ -92,27 +84,14 @@ def cadastrarUsuarioInput():
         else:
             print('Digite apenas s ou m')
 
-def cadastrarUsuario(nome, endereco, telefone):
-    novoUsuario = {
-        "id": uuid.uuid4(),
-        "nome": nome,
-        "endereco": endereco,
-        "telefone": telefone,
-    }
-
-    usuarios_cadastrados.append(novoUsuario)
-
-    return novoUsuario
-
-def visualizarUsuarios():
-    if not usuarios_cadastrados:
+def visualizarUsuariosInput():
+    usuarios = obterUsuarios()
+    if not usuarios:
         print("\nNenhum usuário cadastrado.")
     else:
         print("\nUsuários cadastrados até agora:")
-        
-        for usuario in usuarios_cadastrados:
+        for usuario in usuarios:
             print(f"id: {usuario['id']}, Nome: {usuario['nome']}, Endereço: {usuario['endereco']}, Telefone: {usuario['telefone']}")
-
     while True:
         resp = input("\nVoltar ao menu? (s para sim): ").lower()
         if resp == 's':
@@ -121,31 +100,25 @@ def visualizarUsuarios():
         else:
             print("Digite 's' para voltar ao menu.")
 
-def removerUsuario():
-    if not usuarios_cadastrados:
+def removerUsuarioInput():
+    usuarios = obterUsuarios()
+    if not usuarios:
         print("\nNão há usuários cadastrados para remover.")
         menu()
         return
-
+    
     id = input('\nDigite o id do usuário que deseja deletar: ')
+    usuario_removido = removerUsuario(id)
     
-    usuario_encontrado = None
-    
-    for usuario in usuarios_cadastrados:
-        if str(usuario['id']) == id:
-            usuario_encontrado = usuario
-            break
-
-    if usuario_encontrado:
-        usuarios_cadastrados.remove(usuario_encontrado)
-        print(f"\nUsuário '{usuario_encontrado['nome']}' removido com sucesso!")
+    if usuario_removido:
+        print(f"\nUsuário '{usuario_removido['nome']}' removido com sucesso!")
     else:
         print(f"\nUsuário não encontrado com o id {id}.")
-
-    while True:     
+    
+    while True:
         resp = input('Remover outro usuário (s) ou voltar para o menu (m)?').lower()
         if resp == 's':
-            removerUsuario()
+            removerUsuarioInput()
             break
         elif resp == 'm':
             menu()
@@ -153,73 +126,94 @@ def removerUsuario():
         else:
             print('Digite apenas s ou m')
 
-# Sensores
+# Usuários - Backend
+def cadastrarUsuario(nome, endereco, telefone):
+    novoUsuario = {
+        "id": uuid.uuid4(),
+        "nome": nome,
+        "endereco": endereco,
+        "telefone": telefone,
+    }
+    usuarios_cadastrados.append(novoUsuario)
+    return novoUsuario
+
+def obterUsuarios():
+    return usuarios_cadastrados
+
+def removerUsuario(id):
+    for usuario in usuarios_cadastrados:
+        if str(usuario['id']) == id:
+            usuarios_cadastrados.remove(usuario)
+            return usuario
+    return None
+
+# Sensores - Frontend
 def gestaoDeSensores():
-    while True:    
+    while True:
         print('\nOpcão 1: Adicionar sensores')
         print('Opcão 2: Remover sensores')
         print('Opcão 3: Visualizar sensores')
         print('Opcão 4: Voltar ao menu principal')
-
         resp = input('\nDigite sua opção: ')
-        
         if resp == '1':
-            adicionarSensor()
+            adicionarSensorInput()
             break
         elif resp == '2':
-            removerSensores()
+            removerSensoresInput()
             break
         elif resp == '3':
-            visualizarSensores()
+            visualizarSensoresInput()
             break
         elif resp == '4':
             menu()
             break
         else:
             print('Digite apenas as opções acima!')
-        
-def adicionarSensor():
+
+def adicionarSensorInput():
     nomeSEN = input('\nDigite o nome do sensor: ')
     Local = input('Digite a localização: ')
-
+    
     while True:
-        alertaOBS = int(input('Digite o valor de alerta de nível OBSERVAÇÃO (Entre 0 e 100): '))
-        if alertaOBS < 0 or alertaOBS > 100:
-            print("Valor de alerta OBSERVAÇÃO deve estar entre 0 e 100.")
+        alertaOBS_input = input('Digite o valor de alerta de nível OBSERVAÇÃO (Entre 0 e 100): ')
+        if alertaOBS_input.isdigit():
+            alertaOBS = int(alertaOBS_input)
+            if 0 <= alertaOBS <= 100:
+                break
+            else:
+                print("Valor de alerta OBSERVAÇÃO deve estar entre 0 e 100.")
         else:
-            break
-
+            print("Digite um número inteiro válido para o valor de alerta OBSERVAÇÃO.")    
+    
     while True:
-        alertaATE = int(input('Digite o valor de alerta de nível ATENÇÃO (Entre 0 e 100): '))
-        if alertaATE < 0 or alertaATE > 100:
-            print("Valor de alerta ATENÇÃO deve estar entre 0 e 100.")
+        alertaATE_input = input('Digite o valor de alerta de nível ATENÇÃO (Entre 0 e 100): ')
+        if alertaATE_input.isdigit():
+            alertaATE = int(alertaATE_input)
+            if 0 <= alertaATE <= 100:
+                break
+            else:
+                print("Valor de alerta ATENÇÃO deve estar entre 0 e 100.")
         else:
-            break
-
+            print("Digite um número inteiro válido para o valor de alerta ATENÇÃO.")
+    
     while True:
-        alertaEME = int(input('Digite o valor de alerta de nível EMERGÊNCIA (Entre 0 e 100): '))
-        if alertaEME < 0 or alertaEME > 100:
-            print("Valor de alerta EMERGÊNCIA deve estar entre 0 e 100.")
+        alertaEME_input = input('Digite o valor de alerta de nível EMERGÊNCIA (Entre 0 e 100): ')
+        if alertaEME_input.isdigit():
+            alertaEME = int(alertaEME_input)
+            if 0 <= alertaEME <= 100:
+                break
+            else:
+                print("Valor de alerta EMERGÊNCIA deve estar entre 0 e 100.")
         else:
-            break
+            print("Digite um número inteiro válido para o valor de alerta EMERGÊNCIA.")
 
-    sensor = {
-        "id": uuid.uuid4(),
-        "Nome": nomeSEN,
-        "Localização": Local,
-        "Alerta OBSERVAÇÃO": alertaOBS,
-        "Alerta ATENÇÃO": alertaATE,
-        "Alerta EMERGÊNCIA": alertaEME,
-        "Valor ATUAL": 0,    
-    }
-
-    sensores_cadastrados.append(sensor)
-    print(f"Sensor {nomeSEN} cadastrado com sucesso!\n")
-
+    sensor = adicionarSensor(nomeSEN, Local, alertaOBS, alertaATE, alertaEME)
+    print(f"Sensor {sensor['Nome']} cadastrado com sucesso!\n")
+    
     while True:
         resp = input('Cadastrar outro sensor (s) ou voltar para o menu (m)?').lower()
         if resp == 's':
-            adicionarSensor()
+            adicionarSensorInput()
             break
         elif resp == 'm':
             menu()
@@ -227,30 +221,25 @@ def adicionarSensor():
         else:
             print('Digite apenas s ou m')
 
-def removerSensores():
-    if not sensores_cadastrados:
+def removerSensoresInput():
+    sensores = obterSensores()
+    if not sensores:
         print("\nNão há sensores cadastrados para remover.")
         menu()
         return
-
+    
     id = input("\nDigite o id do sensor que deseja remover: ")
-
-    sensor_encontrado = None
-    for sensor in sensores_cadastrados:
-        if str(sensor['id']) == id:
-            sensor_encontrado = sensor
-            break
-
-    if sensor_encontrado:
-        sensores_cadastrados.remove(sensor_encontrado)
-        print(f"Sensor '{sensor_encontrado['Nome']}' removido com sucesso!")
+    sensor_removido = removerSensor(id)
+    
+    if sensor_removido:
+        print(f"Sensor '{sensor_removido['Nome']}' removido com sucesso!")
     else:
         print("Sensor não encontrado com os dados informados.")
-
+    
     while True:
         resp = input("\nDeseja remover outro sensor (s) ou voltar ao menu (m)? ").lower()
         if resp == 's':
-            removerSensores()
+            removerSensoresInput()
             break
         elif resp == 'm':
             menu()
@@ -258,12 +247,13 @@ def removerSensores():
         else:
             print("Digite apenas 's' ou 'm'.")
 
-def visualizarSensores():
-    if not sensores_cadastrados:
+def visualizarSensoresInput():
+    sensores = obterSensores()
+    if not sensores:
         print("\nNenhum sensor cadastrado.")
     else:
         i = 1
-        for sensor in sensores_cadastrados:
+        for sensor in sensores:
             print(f"\nSensor {i}")
             print("-" * 30)
             print(f"id: {sensor['id']}")
@@ -274,7 +264,7 @@ def visualizarSensores():
             print(f"Alerta EMERGÊNCIA: {sensor['Alerta EMERGÊNCIA']}")
             print(f"Valor ATUAL: {sensor['Valor ATUAL']}")
             i += 1
-
+    
     while True:
         resp = input("\nVoltar ao menu? (s para sim): ").lower()
         if resp == 's':
@@ -283,11 +273,45 @@ def visualizarSensores():
         else:
             print("Digite 's' para voltar ao menu.")
 
-# Alertas
-def ativarSensores():
-    if not sensores_cadastrados:
-        print("\nSem sensores cadastrados para serem ativados.")
+# Sensores - Backend
+def adicionarSensor(nome, localizacao, alertaOBS, alertaATE, alertaEME):
+    sensor = {
+        "id": uuid.uuid4(),
+        "Nome": nome,
+        "Localização": localizacao,
+        "Alerta OBSERVAÇÃO": alertaOBS,
+        "Alerta ATENÇÃO": alertaATE,
+        "Alerta EMERGÊNCIA": alertaEME,
+        "Valor ATUAL": 0,
+    }
+    sensores_cadastrados.append(sensor)
+    return sensor
 
+def obterSensores():
+    return sensores_cadastrados
+
+def removerSensor(id):
+    for sensor in sensores_cadastrados:
+        if str(sensor['id']) == id:
+            sensores_cadastrados.remove(sensor)
+            return sensor
+    return None
+
+def buscarSensorPorId(id):
+    for sensor in sensores_cadastrados:
+        if str(sensor['id']) == id:
+            return sensor
+    return None
+
+def atualizarValorSensor(sensor, valor):
+    sensor['Valor ATUAL'] = valor
+    return sensor
+
+# Alertas - Frontend
+def ativarSensoresInput():
+    sensores = obterSensores()
+    if not sensores:
+        print("\nSem sensores cadastrados para serem ativados.")
         while True:
             resp = input("\nVoltar ao menu? (s para sim): ").lower()
             if resp == 's':
@@ -296,33 +320,30 @@ def ativarSensores():
             else:
                 print("Digite 's' para voltar ao menu.")
     else:
-        for sensor in sensores_cadastrados:
-
+        for sensor in sensores:
             while True:
-                valor_atual = int(input(f"\nDigite o valor atual do sensor '{sensor['Nome']}': "))
-                if valor_atual < 0 or valor_atual > 100:
-                    print("Valor inválido. Digite um valor entre 0 e 100.")
+                valor_atual_input = input(f"\nDigite o valor atual do sensor '{sensor['Nome']}': ")
+                if valor_atual_input.isdigit():
+                    valor_atual = int(valor_atual_input)
+                    if 0 <= valor_atual <= 100:
+                        break
+                    else:
+                        print("Valor inválido. Digite um valor entre 0 e 100.")
                 else:
-                    break
+                    print("Digite um número inteiro válido para o valor atual.")
 
-            sensor['Valor ATUAL'] = valor_atual
-
-            if valor_atual >= sensor['Alerta EMERGÊNCIA']:
-                alertaGerado = gerarAlerta("EMERGÊNCIA", mensagemEmergencia, sensor)
-                print(f"\nAlerta de EMERGÊNCIA gerado e enviado para os usuários cadastrados: {alertaGerado['mensagem']}")
-            elif valor_atual >= sensor['Alerta ATENÇÃO']:
-                alertaGerado = gerarAlerta("ATENÇÃO", mensagemAtencao, sensor)
-                print(f"\nAlerta de ATENÇÃO gerado e enviado para os usuários cadastrados: {alertaGerado['mensagem']}")
-            elif valor_atual >= sensor['Alerta OBSERVAÇÃO']:
-                alertaGerado = gerarAlerta("OBSERVAÇÃO", mensagemObservacao, sensor)
-                print(f"\nAlerta de OBSERVAÇÃO gerado e enviado para os usuários cadastrados: {alertaGerado['mensagem']}")
+            sensor_atualizado = atualizarValorSensor(sensor, valor_atual)
+            alerta_gerado = verificarAlertas(sensor_atualizado)
+            
+            if alerta_gerado:
+                print(f"\nAlerta de {alerta_gerado['nivel']} gerado e enviado para os usuários cadastrados: {alerta_gerado['mensagem']}")
             else:
                 print(f"Sensor '{sensor['Nome']}' está dentro dos limites normais.")
-
+        
         while True:
             resp = input("\nDeseja ativar os sensores novamente (s) ou voltar ao menu (m)? ").lower()
             if resp == 's':
-                ativarSensores()
+                ativarSensoresInput()
                 break
             elif resp == 'm':
                 menu()
@@ -330,24 +351,14 @@ def ativarSensores():
             else:
                 print("Digite apenas 's' ou 'm'.")
 
-def gerarAlerta(nivel, mensagem, sensor):
-    alerta = {
-        "dataHora": datetime.now().strftime('%d/%m/%Y %H:%M'),
-        "nivel": nivel,
-        "mensagem": f'"{mensagem}"',
-        "sensor": sensor
-    }
-
-    alertas.append(alerta)
-    return alerta
-
-def visualizarAlertas():
-    if not alertas:
+def visualizarAlertasInput():
+    alertas_lista = obterAlertas()
+    if not alertas_lista:
         print("\nNenhum alerta gerado.")
     else:
         print("\nAlertas gerados:")
         i = 1
-        for alerta in alertas:
+        for alerta in alertas_lista:
             print(f"\nAlerta {i}")
             print("-" * 30)
             print(f"Data/Hora: {alerta['dataHora']}")
@@ -355,7 +366,7 @@ def visualizarAlertas():
             print(f"Mensagem: {alerta['mensagem']}")
             print(f"Sensor: {alerta['sensor']}")
             i += 1
-
+    
     while True:
         resp = input("\nVoltar ao menu? (s para sim): ").lower()
         if resp == 's':
@@ -364,38 +375,59 @@ def visualizarAlertas():
         else:
             print("Digite 's' para voltar ao menu.")
 
-def gerarAlertaManual():
+def gerarAlertaManualInput():
     while True:
         nivel = input("\nDigite o nível do alerta (EMERGÊNCIA, ATENÇÃO, OBSERVAÇÃO): ").upper()
         if nivel not in ["EMERGÊNCIA", "ATENÇÃO", "OBSERVAÇÃO"]:
             print("Nível de alerta inválido. Deve ser EMERGÊNCIA, ATENÇÃO ou OBSERVAÇÃO.")
         else:
             break
-
+    
     mensagem = input("Digite a mensagem do alerta: ")
     sensor_id = input("Digite o ID do sensor relacionado ao alerta: ")
-
-    sensor_encontrado = None
-    for sensor in sensores_cadastrados:
-        if str(sensor['id']) == sensor_id:
-            sensor_encontrado = sensor
-            break
-
+    
+    sensor_encontrado = buscarSensorPorId(sensor_id)
     if not sensor_encontrado:
         print("Sensor não encontrado.")
     else:
         alertaGerado = gerarAlerta(nivel, mensagem, sensor_encontrado)
         print(f"\nAlerta gerado manualmente e enviado para usuários cadastrados: {alertaGerado['mensagem']}")
-
+    
     while True:
         resp = input("Deseja gerar outro alerta (s) ou voltar ao menu (m)? ").lower()
         if resp == 's':
-            gerarAlertaManual()
+            gerarAlertaManualInput()
             break
         elif resp == 'm':
             menu()
             return
         else:
             print("Digite apenas 's' ou 'm'.")
+
+# Alertas - Backend
+def gerarAlerta(nivel, mensagem, sensor):
+    alerta = {
+        "dataHora": datetime.now().strftime('%d/%m/%Y %H:%M'),
+        "nivel": nivel,
+        "mensagem": f'"{mensagem}"',
+        "sensor": sensor
+    }
+    alertas.append(alerta)
+    return alerta
+
+def obterAlertas():
+    return alertas
+
+def verificarAlertas(sensor):
+    valor_atual = sensor['Valor ATUAL']
+    
+    if valor_atual >= sensor['Alerta EMERGÊNCIA']:
+        return gerarAlerta("EMERGÊNCIA", mensagemEmergencia, sensor)
+    elif valor_atual >= sensor['Alerta ATENÇÃO']:
+        return gerarAlerta("ATENÇÃO", mensagemAtencao, sensor)
+    elif valor_atual >= sensor['Alerta OBSERVAÇÃO']:
+        return gerarAlerta("OBSERVAÇÃO", mensagemObservacao, sensor)
+    else:
+        return None
 
 main()
